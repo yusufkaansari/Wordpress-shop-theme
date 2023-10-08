9b30ac616efde140823e37638f722cd2c4876c72
Yusuf Kaan SARI <yusufkaansari@hotmail.com>
2023-10-08 15:00:15 +0300

    Added Blog Theme, html codes to php files

---
+++ wp-content/themes/blog_wp/footer.php
@@ -0,0 +1,10 @@
+</div> <!--govde-->
+
+<div class="alt">
+Alt kısım. (c) 2015
+</div>
+</div>
+
+
+</body>
+</html>
---
+++ wp-content/themes/blog_wp/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>anasayfa</title>
+	<link rel="stylesheet" type="text/css" href="reset.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="site">
+	<div class="ust">
+		<div class="logo">
+			<h2><a href="">Udemy Tema</a></h2>
+		</div>
+
+		<div class="menu">
+			<ul>
+				<li><a href="">Anasayfa</a></li>
+				<li><a href="">İletişim</a></li>
+				<li><a href="">Örnek Sayfa</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="govde">
